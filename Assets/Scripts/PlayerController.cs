7833dd1d638a6591eb4892b6e7ae9c2b3e80ab98
deancohen1234 <dean3712@gmail.com>
2023-07-07 19:22:37 -0600

    Swapped to mouse controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Vector2 range = new Vector2(-5f, 5f);
 
+    private Vector3 startingPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -23,13 +25,23 @@ public class PlayerController : MonoBehaviour
     {
         float xInput = Input.GetAxis("Horizontal");
 
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+
         Vector3 positionOffset = new Vector3(xInput * -moveSpeed, 0, 0) * Time.deltaTime;
 
         //limit position
         Vector3 newPosition = transform.position + positionOffset;
         newPosition.x = Mathf.Clamp(newPosition.x, range.x, range.y);
 
-        transform.position = newPosition;
+        //transform.position = newPosition;
+
+        worldPoint.x = Mathf.Clamp(worldPoint.x, range.x, range.y);
+        worldPoint.y = startingPosition.y;
+        worldPoint.z = startingPosition.z;
+
+        transform.position = worldPoint;
+
 
     }
 }
